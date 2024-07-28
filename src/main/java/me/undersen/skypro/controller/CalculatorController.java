f3caac89c21949e0f068b5d10d18fd162449ef08
UndersenF <begintolife@mail.ru>
2024-07-29 00:41:11 +0300

    hw is ready!

--- src/main/java/me/undersen/skypro/controller/CalculatorController.java
+++ src/main/java/me/undersen/skypro/controller/CalculatorController.java
@@ -11,4 +11,39 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/calculator")
 public class CalculatorController {
 
+    private CalculatorService calculatorService;
+
+    @Autowired
+    public CalculatorController(CalculatorService calculatorService) {
+        this.calculatorService = calculatorService;
+    }
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return calculatorService.sayHello();
+    }
+
+    @GetMapping("/plus")
+    public String plus(@RequestParam int num1,
+                       @RequestParam int num2) {
+        return calculatorService.summation(num1, num2);
+    }
+
+    @GetMapping("/minus")
+    public String minus(@RequestParam int num1,
+                       @RequestParam int num2) {
+        return calculatorService.subtraction(num1, num2);
+    }
+
+    @GetMapping("/divide")
+    public String divide(@RequestParam int num1,
+                       @RequestParam int num2) {
+        return calculatorService.division(num1, num2);
+    }
+
+    @GetMapping("/multiply")
+    public String multiply(@RequestParam int num1,
+                       @RequestParam int num2) {
+        return calculatorService.multiplication(num1, num2);
+    }
 }
--- src/main/java/me/undersen/skypro/service/CalculatorService.java
+++ src/main/java/me/undersen/skypro/service/CalculatorService.java
@@ -5,6 +5,26 @@ import org.springframework.stereotype.Component;
 @Component
 public class CalculatorService {
 
+    public String sayHello() {
 
+        return "Добро пожаловать в калькулятор!";
+    }
+
+    public String summation (int num1, int num2 ) {
+        String summary = String.valueOf(num1) + " + " + String.valueOf(num2) + " = " + (num1 + num2);
+        return summary;
+    }
+    public String subtraction (int num1, int num2 ) {
+        String difference = String.valueOf(num1) + " - " + String.valueOf(num2) + " = " + (num1 - num2);
+        return difference;
+    }
+    public String division (int num1, int num2 ) {
+        String divide = String.valueOf(num1) + " / " + String.valueOf(num2) + " = " + (num1 / num2);
+        return divide;
+    }
+    public String multiplication (int num1, int num2 ) {
+        String multiply = String.valueOf(num1) + " * " + String.valueOf(num2) + " = " + (num1 * num2);
+        return multiply;
+    }
 
 }
