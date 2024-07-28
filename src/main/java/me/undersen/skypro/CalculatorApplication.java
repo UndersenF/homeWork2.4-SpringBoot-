c5e4912d8edf762dbd4eed39f1fd63a408a8b8ec
UndersenF <begintolife@mail.ru>
2024-07-29 00:39:19 +0300

    initial commit!

---
+++ src/main/java/me/undersen/skypro/CalculatorApplication.java
@@ -0,0 +1,13 @@
+package me.undersen.skypro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalculatorApplication.class, args);
+    }
+
+}
---
+++ src/main/java/me/undersen/skypro/service/CalculatorService.java
@@ -0,0 +1,10 @@
+package me.undersen.skypro.service;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class CalculatorService {
+
+
+
+}
